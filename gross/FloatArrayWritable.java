a64049d464774c4e7223c379ee0b3da8d8cf98b4
albertoandreotti@gmail.com <albertoandreotti@gmail.com@fbbb0e93-9395-4ede-d7dd-4dc894e52f69>
2011-03-20 16:41:21 +0000

    More corrrections!

--- gross/FloatArrayWritable.java
+++ gross/FloatArrayWritable.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class FloatArrayWritable extends ArrayWritable
 {
-
+    int len;
     public FloatArrayWritable() {
         super(FloatWritable.class);
     }
@@ -13,6 +13,7 @@ public class FloatArrayWritable extends ArrayWritable
     }
 	
     public FloatArrayWritable(int length) {
+        this.len=length;
         super(FloatWritable.class);
 	FloatWritable[] FloatArray = new FloatWritable[length];  
         for(int i = 0; i<length; i++){
@@ -28,7 +29,16 @@ public class FloatArrayWritable extends ArrayWritable
     public void set(int i, float val) {
         ((FloatWritable)(this.get())[i]).set(val);
     }
+    public void setHeatSource(float heat, int pos){
 
+        //TODO:check pos
+        for(int i = 0; i<length; i++){
+            FloatArray[i] = new FloatWritable(0f);
+        }
+        if(pos>len) return;
+        FloatArray[pos]= new FloatWritable(heat);
+        this.set(FloatArray);
+  }
 
 
     public int compareTo(Object o) {
